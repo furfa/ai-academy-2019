ff7dd9b247202b2c6e17a259db2070c2534108a8
aleksandr <aleksandr.myashin@gmail.com>
2019-04-22 03:43:53 +0700

    refactor

--- Scripts/saving.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-import os
-
-def submit(pred, base_name="", pred_path="/home/furfa/work/ai-academy2019/predictions"):
-    date = str(datetime.now())
-    name = f"{base_name}[{date}].csv"
-    path = os.path.join(pred_path, name)
-    pred.to_csv(path, index = None) # 40 баллов
-    print("File saved in :",path)
